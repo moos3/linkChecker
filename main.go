0092e1196bfd84d8808250ee2ff2e21ec16cb8be
Richard Genthner <richard.genthner@stalingrad.local>
2018-06-20 12:41:02 -0400

    refactored the code and added a timeout of 30 seconds to the client. Also made it not be fatal if url has to many redirects, instead just being fatal

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"gopkg.in/cheggaaa/pb.v1"
 )
@@ -17,6 +18,9 @@ type Node struct {
 	statusCode  int
 }
 
+// readLines -
+// This reads a file of urls and will remove ones that are dupicates.
+// This func is very dirty and could be smartter.
 func readLines(path string) ([]string, error) {
 	file, err := os.Open(path)
 	if err != nil {
@@ -44,12 +48,19 @@ func readLines(path string) ([]string, error) {
 	return result, scanner.Err()
 }
 
+// urlCheck -
+// This function is for URL checking and will return a single Node
 func urlCheck(url string) (Node, error) {
 
 	var data Node
-	resp, err := http.Get(url)
+
+	timeout := time.Duration(30 * time.Second)
+	client := http.Client{
+		Timeout: timeout,
+	}
+	resp, err := client.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 	data.link = url
 	data.statusCode = resp.StatusCode
@@ -59,6 +70,26 @@ func urlCheck(url string) (Node, error) {
 
 	return data, err
 
+}
+
+// runChecker - This just loops over a slice of strings
+func runChecker(l []string) []Node {
+	checked := []Node{}
+	fmt.Println("Checking Urls")
+	bar := pb.StartNew(len(l))
+	// crawl each website in input file one consecutively
+	for i := 0; i < len(l); i++ {
+		bar.Increment()
+		n, e := urlCheck(l[i])
+		if e != nil {
+			fmt.Printf("Got Error: %s when fetching: %s", e, l[i])
+		} else {
+			checked = append(checked, n)
+		}
+
+	}
+
+	return checked
 }
 
 func main() {
@@ -71,25 +102,12 @@ func main() {
 		fmt.Println("Error reading file: ", err)
 	}
 
-	checked := []Node{}
-	bar := pb.StartNew(len(links))
-	fmt.Println("Checking Urls")
-	// crawl each website in input file one consecutively
-	for i := 0; i < len(links); i++ {
-		bar.Increment()
-		l, e := urlCheck(links[i])
-		if e != nil {
-			fmt.Printf("Got Error: %s when fetching: %s", e, links[i])
-		} else {
-			checked = append(checked, l)
-		}
+	checked := runChecker(links)
 
-	}
 	fmt.Println("Reported 404 urls")
 	for _, link := range checked {
 		if link.statusCode == 404 {
 			fmt.Printf("Url: %s, reported: %d\n", link.link, link.statusCode)
-			//fmt.Println(link)
 		}
 	}
 
